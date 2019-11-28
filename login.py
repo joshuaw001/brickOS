1673e6b00b49c33336451fa86546c5fd49acc095
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-11-28 17:36:04 -0600

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 from guizero import *
 import os
 
-app         = App(layout="grid", bg="white")
+app = App(layout="grid", bg="white")
 
 def login():
     if password == pinInput.value_text:
@@ -19,5 +19,7 @@ pinError    = Text(app, grid=[0,1], text="You must enter \n a PIN to login")
 pinError.fg = "white"
 pinError.bg = "white"
 PinEnter    = PushButton(app, text="LOGIN", command=login())
+
+
 
 app.display()
