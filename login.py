e89b46762b5761608de3e0de5c0a82edcd2b5ea7
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-11-28 12:08:08 -0600

    Update login.py

--- login.py
+++ login.py
@@ -1,12 +1,3 @@
-#logging into the OS
-
-import PySimpleGUI as sg
+from guizero import *
 import os
 
-layout = [
-    [sg.Text("PIN ID:"), sg.InputText()],
-    [sg.Text("PASSWORD:"), sg.InputText()],
-    [sg.Button("USERS LOGIN"), sg.Button("GUEST LOGIN")]
-]
-
-sg.Window("Login to brickOS").Layout(layout)
